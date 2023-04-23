8ffa66ea2e39fd3c9b3339add0c1ed8bf9590148
sefirus <sashasam348@gmail.com>
2023-04-23 16:59:54 +0300

    Started work on table order page

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ app_name = 'orders'
 urlpatterns = [
     # other URL patterns ...
     path('create-table-order/', views.create_table_order, name='create_table_order'),
+    path('table_order/<uuid:table_order_id>/', views.table_order, name='table_order'),
+    path('table_order/<uuid:table_order_id>/<uuid:personal_order_id>/', views.table_order, name='table_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 from orders.forms import CreateTableOrderForm
+from orders.models import TableOrder, PersonalOrder
 
 
 # Create your views here.
+@login_required
 def create_table_order(request):
     request.hide_header_links = True
     request.hide_call_waiter = True
@@ -18,3 +21,23 @@ def create_table_order(request):
     else:
         form = CreateTableOrderForm()
     return render(request, 'create_table_order.html', {'form': form})
+
+
+@login_required
+def table_order(request, table_order_id, personal_order_id=None):
+    request.hide_header_links = True
+    request.hide_call_waiter = True
+    table_order = get_object_or_404(TableOrder, id=table_order_id)
+    personal_orders = table_order.personal_orders.all()
+
+    if personal_order_id:
+        selected_personal_order = get_object_or_404(PersonalOrder, id=personal_order_id)
+    else:
+        selected_personal_order = personal_orders.first() if personal_orders else None
+
+    context = {
+        'table_order': table_order,
+        'personal_orders': personal_orders,
+        'selected_personal_order': selected_personal_order
+    }
+    return render(request, 'table_order.html', context)
