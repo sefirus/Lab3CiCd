bdb72c25c705b9bdf032f0c493247cff278d6a5c
sefirus <sashasam348@gmail.com>
2023-04-26 00:11:20 +0300

    Implemented adding items to order

--- orders/urls.py
+++ orders/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('create-table-order/', views.create_table_order, name='create_table_order'),
     path('table_order/<uuid:table_order_id>/', views.table_order, name='table_order'),
     path('table_order/<uuid:table_order_id>/<uuid:personal_order_id>/', views.table_order, name='table_order'),
-    path('table_order/<uuid:table_order_id>/create_personal_order/', views.create_personal_order, name='create_personal_order'),
+    path('table_order/<uuid:table_order_id>/create_personal_order/', views.create_personal_order,
+         name='create_personal_order'),
+    path('table_order/<uuid:table_order_id>/<uuid:personal_order_id>/add_menu_item/', views.add_menu_item,
+         name='add_menu_item'),
+
 ]
