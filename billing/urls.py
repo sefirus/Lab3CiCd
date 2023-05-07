00598fafdacbaddf7fff076b125b8e3d1ed3583a
sefirus <sashasam348@gmail.com>
2023-05-07 08:39:33 +0300

    Fixed filters mock

--- billing/urls.py
+++ billing/urls.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path
 
 from billing import views
 
@@ -7,7 +6,6 @@ from billing import views
 app_name = 'billing'
 urlpatterns = [
     path('', views.index, name='index_no_number'),
-    #path('menu/', include('menu.urls')),
     path('<int:table_number>/', views.index, name='index'),
     path('call_waiter/', views.call_waiter, name='call_waiter'),
     path('client_checkout/<int:table_number>', views.client_checkout, name='client_checkout'),
--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from menu import views
 
 app_name = 'menu'
 urlpatterns = [
-    path('/', views.menu, name='menu'),
+    path('', views.menu, name='menu'),
 ]
