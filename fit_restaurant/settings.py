441e2b4925fbad7d9e49d52b84c6c742b78eb588
sefirus <sashasam348@gmail.com>
2023-05-15 23:37:13 +0300

    fix statics

--- fit_restaurant/settings.py
+++ fit_restaurant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qq*!ghz(^r7@(b%bogm*^qc*f1w6l&hm5ttxhcud_g67zy-+oa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fit-restaurant.herokuapp.com']
+ALLOWED_HOSTS = ['fit-restaurant.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
